6c7c8dcfa54495502fdbfe450131031dcccc20d9
Brendan Luchen <whyareyouspammingme@gmail.com>
2010-07-26 14:55:06 -0400

    More Doxygen automation

--- cmake-modules/UseDoxygen.cmake
+++ cmake-modules/UseDoxygen.cmake
@@ -22,11 +22,14 @@
 #
 #
 # Variables you may define are:
+#  DOXYFILE_SOURCE_DIR - Path where the Doxygen input files are. Defaults to the current binary dir.
 #  DOXYFILE_OUTPUT_DIR - Path where the Doxygen output is stored. Defaults to "doc".
 #
-#  DOXYFILE_LATEX_DIR - Directory where the Doxygen LaTeX output is stored. Defaults to "latex".
+#  DOXYFILE_LATEX_DIR - Directory relative to DOXYFILE_OUTPUT_DIR where
+#  the Doxygen LaTeX output is stored. Defaults to "latex".
 #
-#  DOXYFILE_HTML_DIR - Directory where the Doxygen html output is stored. Defaults to "html".
+#  DOXYFILE_HTML_DIR - Directory relative to DOXYFILE_OUTPUT_DIR where
+#  the Doxygen html output is stored. Defaults to "html".
 #
 
 #
@@ -45,31 +48,31 @@ macro(usedoxygen_set_default name value)
 endmacro()
 
 find_package(Doxygen)
+find_package(LATEX)
 
 if(DOXYGEN_FOUND)
-	find_file(DOXYFILE_IN "Doxyfile.in"
-			PATHS "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_ROOT}/Modules/")
+	find_path(DOXYFILE_DIR "Doxyfile.in"
+			PATHS "${CMAKE_SOURCE_DIR}" "${CMAKE_ROOT}/Modules/")
+	set(DOXYFILE_IN "${DOXYFILE_DIR}/Doxyfile.in")
+	set(DOXYFILE "${CMAKE_CURRENT_BINARY_DIR}/Doxyfile")
 
 	include(FindPackageHandleStandardArgs)
 	find_package_handle_standard_args(Doxyfile.in DEFAULT_MSG DOXYFILE_IN)
 endif()
 
 if(DOXYGEN_FOUND AND DOXYFILE_IN)
-	add_custom_target(doxygen 
-		COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} )
 
 	usedoxygen_set_default(DOXYFILE_OUTPUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/doc")
 	usedoxygen_set_default(DOXYFILE_HTML_DIR "html")
+	usedoxygen_set_default(DOXYFILE_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR})
 
 	set_property(DIRECTORY APPEND PROPERTY
-			ADDITIONAL_MAKE_CLEAN_FILES "${DOXYFILE_OUTPUT_DIR}/${DOXYFILE_HTML_DIR}")
+			ADDITIONAL_MAKE_CLEAN_FILES "${DOXYFILE_OUTPUT_DIR}")
 
 	set(DOXYFILE_LATEX "NO")
 	set(DOXYFILE_PDFLATEX "NO")
 	set(DOXYFILE_DOT "NO")
 
-	find_package(LATEX)
 	if(LATEX_COMPILER AND MAKEINDEX_COMPILER)
 		set(DOXYFILE_LATEX "YES")
 		usedoxygen_set_default(DOXYFILE_LATEX_DIR "latex")
@@ -92,7 +95,12 @@ if(DOXYGEN_FOUND AND DOXYFILE_IN)
 	endif()
 
 
-	configure_file(${DOXYFILE_IN} Doxyfile ESCAPE_QUOTES IMMEDIATE @ONLY)
+	configure_file(${DOXYFILE_IN} ${DOXYFILE} ESCAPE_QUOTES IMMEDIATE @ONLY)
+
+	add_custom_target(doxygen 
+		COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --switch=${CMAKE_COLOR_MAKEFILE} --red --bold "Writing documentation to ${DOXYFILE_OUTPUT_DIR}..."
+		COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE}
+		WORKING_DIRECTORY ${DOXYFILE_SOURCE_DIR} )
 
 	get_target_property(DOC_TARGET doc TYPE)
 	if(NOT DOC_TARGET)
