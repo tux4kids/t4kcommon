78f6956190ed32b87362617d5ab4c6945e01c582
David Bruce <davidstuartbruce@gmail.com>
2010-10-24 05:18:17 -0500

    Updated mingw_cross_env .mk file for 0.0.1

--- t4k_common.mk
+++ t4k_common.mk
@@ -4,14 +4,14 @@
 # t4k_common
 PKG             := t4k_common
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.0.0
-$(PKG)_CHECKSUM := 07b29aededd83b3c386533ff6a2797fe3fcf3374
+$(PKG)_VERSION  := 0.0.1
+$(PKG)_CHECKSUM := eb88b8e0ffc7a7aa1a666b6fff10bff4a7cffedc  
 $(PKG)_SUBDIR   := t4k_common-$($(PKG)_VERSION)
 $(PKG)_FILE     := t4k_common-$($(PKG)_VERSION).tar.gz
 $(PKG)_WEBSITE  := http://tux4kids.alioth.debian.org/
 
 # FIXME find out url that can be specified just with package name and version
-$(PKG)_URL      := https://alioth.debian.org/frs/download.php/3409/t4k_common-0.0.0.tar.gz
+$(PKG)_URL      := https://alioth.debian.org/frs/download.php/3426/t4k_common-0.0.1.tar.gz 
  
 $(PKG)_DEPS     := gcc sdl sdl_mixer sdl_image sdl_net sdl_pango sdl_ttf librsvg libxml2
 
