4345b64ea3a1c87d370c80de30f054d665d3cee7
David Bruce <davidstuartbruce@gmail.com>
2010-10-27 14:26:20 -0500

    Partially updated t4k_common.mk for 0.0.3
    Still need correct Alioth download url

--- t4k_common.mk
+++ t4k_common.mk
@@ -4,8 +4,8 @@
 # t4k_common
 PKG             := t4k_common
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.0.2
-$(PKG)_CHECKSUM := c1a7ef8af196021348506f3fa218f499988cdb1f
+$(PKG)_VERSION  := 0.0.3
+$(PKG)_CHECKSUM := f85c0cb099b975bd90d7ca36446f63d6d73cb614
 $(PKG)_SUBDIR   := t4k_common-$($(PKG)_VERSION)
 $(PKG)_FILE     := t4k_common-$($(PKG)_VERSION).tar.gz
 $(PKG)_WEBSITE  := http://tux4kids.alioth.debian.org/
@@ -13,7 +13,7 @@ $(PKG)_WEBSITE  := http://tux4kids.alioth.debian.org/
 # FIXME find out url that can be specified just with package name and version
 $(PKG)_URL      := https://alioth.debian.org/frs/download.php/3426/t4k_common-0.0.1.tar.gz 
  
-$(PKG)_DEPS     := gcc sdl sdl_mixer sdl_image sdl_net sdl_pango sdl_ttf librsvg libxml2
+$(PKG)_DEPS     := gcc sdl sdl_mixer sdl_image sdl_net sdl_pango sdl_ttf librsvg libxml2 pthreads
 
 # FIXME find out how to update package
 #define $(PKG)_UPDATE
