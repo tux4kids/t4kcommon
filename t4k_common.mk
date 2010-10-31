ea88b7ef09815c6aba3a278df5facd318ad30dbf
David Bruce <davidstuartbruce@gmail.com>
2010-10-31 15:41:25 -0500

    Added corrected download url for 0.0.3

--- t4k_common.mk
+++ t4k_common.mk
@@ -11,7 +11,7 @@ $(PKG)_FILE     := t4k_common-$($(PKG)_VERSION).tar.gz
 $(PKG)_WEBSITE  := http://tux4kids.alioth.debian.org/
 
 # FIXME find out url that can be specified just with package name and version
-$(PKG)_URL      := https://alioth.debian.org/frs/download.php/3426/t4k_common-0.0.1.tar.gz 
+$(PKG)_URL      := https://alioth.debian.org/frs/download.php/3439/t4k_common-0.0.3.tar.gz 
  
 $(PKG)_DEPS     := gcc sdl sdl_mixer sdl_image sdl_net sdl_pango sdl_ttf librsvg libxml2 pthreads
 
