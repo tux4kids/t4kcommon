a7f8ec3705dc4a97eeffea5266bf4b61811112af
David Bruce <davidstuartbruce@gmail.com>
2011-02-24 19:24:14 -0600

    Use T4K_BlackOutline() for tooltips

--- src/t4k_menu.c
+++ src/t4k_menu.c
@@ -830,6 +830,7 @@ int T4K_RunMenu(int index, bool return_choice, void (*draw_background)(), int (*
 	      char_width = desc_chars_per_line(T4K_TOOLTIP_FONTSIZE);
               T4K_LineWrapInsBreaks(desc, out, char_width, 64, 64);
               desc_prerendered = T4K_SimpleText(out, T4K_TOOLTIP_FONTSIZE, &yellow);
+              desc_prerendered = T4K_BlackOutline(out, T4K_TOOLTIP_FONTSIZE, &yellow);
 	    }
           }
           old_loc = loc;
