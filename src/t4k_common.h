tux4kids/t4kcommon

commit f36ec174f4d8130749f11aeb1d77a81da4be89aa

Fix wrapped_lines variable declaration (extern)
Signed-off-by: Holger Levsen <holger@layer-acht.org>

src/t4k_common.h
@@ -245,7 +245,7 @@ MFStrategy;
 #define MAX_LINES           128 //!< Maximum lines to wrap.
 #define MAX_LINEWIDTH       256 //!< Maximum characters of each line.
 
-static char wrapped_lines[MAX_LINES][MAX_LINEWIDTH]; //!< Global buffer for wrapped lines.
+extern static char wrapped_lines[MAX_LINES][MAX_LINEWIDTH]; //!< Global buffer for wrapped lines.
 
 //TODO separate headers for different areas a la SDL?
 

commit a65a28eda3bc09dd1e800e36f5611b352a42fec8

linewrap: Fix declaration of wrapped_lines
f36ec174f4d8130749f11aeb1d77a81da4be89aa introduced a compile error
wherever this header is included:

    t4k_common.h:248:1: error: multiple storage classes in declaration specifiers
      248 | extern static char wrapped_lines[MAX_LINES][MAX_LINEWIDTH]; //!< Global buffer for wrapped lines.
          | ^~~~~~

A variable can't be both extern and static. The variable must be
declared extern in a header file (which is included in, and compiled as
part of, many source files) but defined in exactly one source file in
this library.

(This variable is not used within this library; but is provided for the
convenience of library users and is used by, at least, tuxmath.)

See discussion on
https://github.com/tux4kids/t4kcommon/commit/f36ec174f4d8130749f11aeb1d77a81da4be89aa.

src/t4k_common.h
@@ -245,7 +245,7 @@ MFStrategy;
 #define MAX_LINES           128 //!< Maximum lines to wrap.
 #define MAX_LINEWIDTH       256 //!< Maximum characters of each line.
 
-extern static char wrapped_lines[MAX_LINES][MAX_LINEWIDTH]; //!< Global buffer for wrapped lines.
+extern char wrapped_lines[MAX_LINES][MAX_LINEWIDTH]; //!< Global buffer for wrapped lines.
 
 //TODO separate headers for different areas a la SDL?
 

src/t4k_linewrap.c
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 #include <stdio.h>
 
 static char wrapped_lines0[MAX_LINES][MAX_LINEWIDTH];  // for internal storage
-//char wrapped_lines[MAX_LINES][MAX_LINEWIDTH]; // publicly available!
+extern char wrapped_lines[MAX_LINES][MAX_LINEWIDTH]; // publicly available!
 
 
 